7e45e22b304ec8dc0abfc6cb81ebc50fe50a5616
devzubair1 <dazzlepk98@gmail.com>
2023-09-15 16:28:43 +0500

    First Exercise

---
+++ Index.js
@@ -0,0 +1,17 @@
+"use strict";
+//Q.1
+var message = "Hello Zubair Would you like to learn Typescript Program";
+console.log(message);
+//Q.2
+var personName = "Zubair Arshad";
+console.log(personName);
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+//Q.3
+var auther_Name = "Muhammad Ali Jinnah";
+var quotes = "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.";
+console.log(`${auther_Name}, Once Said: ${quotes} `);
+//Q.4
+var famous_person = "Allama Mohammad Iqbal";
+var massage = "A man without a vision is a man without a future. A man without a future will always return to his past.";
+console.log(`${massage} By Allama Mohammad Iqbal`);
---
+++ Index.ts
@@ -0,0 +1,23 @@
+//Q.1
+var message:string="Hello Zubair Would you like to learn Typescript Program"
+console.log(message);
+
+//Q.2
+var personName:string= "Zubair Arshad"
+console.log(personName);
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+
+//Q.3
+var auther_Name:string= "Muhammad Ali Jinnah"
+var quotes:string= "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve."
+console.log(`${auther_Name}, Once Said: ${quotes} `);
+
+//Q.4
+var famous_person:string= "Allama Mohammad Iqbal"
+var massage:string= "A man without a vision is a man without a future. A man without a future will always return to his past."
+console.log(`${massage} By Allama Mohammad Iqbal`)
+
+//Q.5
+
+
